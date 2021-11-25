7bf39e6ac86f8c31c75523fd9dc48bc1cca16748
Bill DeLuca <bill@textus.com>
2021-11-25 10:00:01 -0700

    Update script to add the triplet report
    The second part of the challenge required looking for a triplet sum. An
    addition function and an extra dependency were added to accomplish this.

--- rust/day-01/src/main.rs
+++ rust/day-01/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use std::path::Path;
+use combinations::Combinations;
 
 fn input_file(filename: impl AsRef<Path>) -> Vec<u32> {
     let file = File::open(filename).expect("no such file");
@@ -10,14 +11,28 @@ fn input_file(filename: impl AsRef<Path>) -> Vec<u32> {
         .collect()
 }
 
-fn repair_report(seek: u32, input: Vec<u32>) -> u32 {
+fn repair_report(seek: u32, input: &Vec<u32>) -> u32 {
     let mut evaluated: Vec<u32> = Vec::new();
     for value in input {
         let diff: u32 = seek - value;
         if evaluated.contains(&diff) {
             return value * diff
         } else {
-            evaluated.push(value)
+            evaluated.push(*value)
+        }
+    }
+    0
+}
+
+fn repair_report_cubed(seek: u32, input: &Vec<u32>) -> u32 {
+    let mut unique_input = input.clone();
+    unique_input.sort();
+    unique_input.dedup();
+    let combinations: Vec<_> = Combinations::new(unique_input, 3).collect();
+    for comb in combinations {
+        let sum: u32 = comb.iter().sum();
+        if sum == seek {
+            return comb[0] * comb[1] * comb[2]
         }
     }
     0
@@ -25,7 +40,8 @@ fn repair_report(seek: u32, input: Vec<u32>) -> u32 {
 
 fn main() {
     let input = input_file("../../input/day-01/input");
-    println!("{:?}", repair_report(2020, input));
+    println!("Repair Report: {:?}", repair_report(2020, &input));
+    println!("Cubed  Report: {:?}", repair_report_cubed(2020, &input));
 }
 
 #[cfg(test)]
@@ -33,9 +49,15 @@ mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
+    const EXAMPLE_INPUT: [u32; 6] = [ 1721, 979, 366, 299, 675, 1456 ];
+
     #[test]
     fn test_repair_report() {
-        let example_input: [u32; 6] = [ 1721, 979, 366, 299, 675, 1456 ];
-        assert_eq!(514579, repair_report(2020, example_input.to_vec()));
+        assert_eq!(514579, repair_report(2020, &EXAMPLE_INPUT.to_vec()));
+    }
+
+    #[test]
+    fn test_repair_report_cubed() {
+        assert_eq!(241861950, repair_report_cubed(2020, &EXAMPLE_INPUT.to_vec()));
     }
 }
